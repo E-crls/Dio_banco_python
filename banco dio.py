ec2b00750336df5ea8dbc2984e136c8c26538e58
Anderson <102735794+E-crls@users.noreply.github.com>
2023-07-01 12:20:41 -0300

    Add files via upload

--- banco dio.py
+++ banco dio.py
@@ -3,41 +3,44 @@ class ContaBancaria:
         self.numero_da_conta = numero_da_conta
         self.saldo = 0
 
-    def deposito(self, valor):
+    def depositar(self, valor):
+        if valor < 0:
+            return "O valor do depósito deve ser positivo."
         self.saldo += valor
-        return self.saldo
+        return f"Depósito realizado com sucesso. Saldo atual: {self.saldo}"
 
-    def saque(self, valor):
+    def sacar(self, valor):
+        if valor < 0:
+            return "O valor do saque deve ser positivo."
         if self.saldo < valor:
-            print("Saldo insuficiente.")
-            return
+            return "Saldo insuficiente para o saque."
         self.saldo -= valor
-        return self.saldo
+        return f"Saque realizado com sucesso. Saldo atual: {self.saldo}"
 
-    def extrato(self):
-        return self.saldo
+    def obter_extrato(self):
+        return f"Saldo atual: {self.saldo}"
 
 class Banco:
     def __init__(self):
         self.contas = {}
 
     def criar_conta(self, numero_da_conta):
+        if numero_da_conta in self.contas:
+            return "Número de conta já existe."
         self.contas[numero_da_conta] = ContaBancaria(numero_da_conta)
+        return f"Conta {numero_da_conta} criada com sucesso."
 
-    def deposito(self, numero_da_conta, valor):
+    def realizar_deposito(self, numero_da_conta, valor):
         if numero_da_conta not in self.contas:
-            print("Conta não encontrada.")
-            return
-        self.contas[numero_da_conta].deposito(valor)
+            return "Conta não encontrada."
+        return self.contas[numero_da_conta].depositar(valor)
 
-    def saque(self, numero_da_conta, valor):
+    def realizar_saque(self, numero_da_conta, valor):
         if numero_da_conta not in self.contas:
-            print("Conta não encontrada.")
-            return
-        self.contas[numero_da_conta].saque(valor)
+            return "Conta não encontrada."
+        return self.contas[numero_da_conta].sacar(valor)
 
-    def extrato(self, numero_da_conta):
+    def obter_extrato(self, numero_da_conta):
         if numero_da_conta not in self.contas:
-            print("Conta não encontrada.")
-            return
-        return self.contas[numero_da_conta].extrato()
+            return "Conta não encontrada."
+        return self.contas[numero_da_conta].obter_extrato()
